31e5a1140011a6249e80f082c86478fb834cbbd7
Jean Ediel de Mello <jean.mello@intranet.lojasmm.com.br>
2025-09-10 20:29:46 -0300

    Configurando Middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,22 @@
-import express, { Express, NextFunction, Request, Response } from 'express';
+import express, { Express } from 'express';
 import { routes } from './routes/routes';
-import morgan from 'morgan';
 import cors from 'cors';
-import fs from 'fs';
-import path from 'path';
+
+import { log } from './middleware/log.middleware';
+import { errorHandling } from './middleware/error-handling.middleware';
 
 const app: Express = express();
-
-const fileLog = fs.createWriteStream(path.join(__dirname, 'storage', 'access.log'), { flags: 'a' });
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(morgan('combined', { stream: fileLog }));
+app.use(log);
 
 // configuração das rotas
 app.use(routes);
 
 // Tratativa de erros da aplicação
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    if (err) {
-        return res.status(400).json({
-            msg: 'Ocorreu um erro!',
-            error: err.message
-        });
-    }
-});
+app.use(errorHandling);
 
 export { app };
